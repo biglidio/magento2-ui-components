c22737d9e8770f0df102397fbed348fb08b7fc05
Lucas Figueiredo <lucas-fj@hotmail.com>
2021-11-06 12:43:01 +0100

    Update UI Component with model data

--- view/frontend/web/js/free-shipping-banner.js
+++ view/frontend/web/js/free-shipping-banner.js
@@ -1,15 +1,17 @@
 define([
+    'ko',
     'uiComponent',
-    'ko'
+    'Magento_Customer/js/customer-data'
 ], function(
+    ko,
     Component,
-    ko
+    customerData
 ) {
     'use strict';
 
     return Component.extend({
         defaults: {
-            subtotal: 33.00,
+            subtotal: 0.00,
             template: "Biglidio_FreeShippingPromo/free-shipping-banner",
             tracks: {
                 subtotal: true
@@ -17,8 +19,13 @@ define([
         },
         initialize: function() {
             this._super();
+            
+            var self = this;
+            var cart = customerData.get('cart');
 
-            console.log(this.message);
+            customerData.getInitCustomerData().done(function() {
+                self.subtotal = parseFloat(cart().subtotalAmount);
+            });
         },
         formatCurrency: function(value) {
             return '$' + value.toFixed(2);
