d45d1e853814622a79b1f83e4680ce04d5768071
Lucas Figueiredo <lucas-fj@hotmail.com>
2021-11-06 16:20:51 +0100

    Create computed properties with KnockoutJS

--- view/frontend/web/js/free-shipping-banner.js
+++ view/frontend/web/js/free-shipping-banner.js
@@ -1,5 +1,5 @@
 define([
-    'ko',
+    'knockout',
     'uiComponent',
     'Magento_Customer/js/customer-data',
     'underscore'
@@ -13,11 +13,10 @@ define([
 
     return Component.extend({
         defaults: {
-            message: '${ $.messageDefault }',
+            freeShippingThreshold: 100,
             subtotal: 0.00,
             template: "Biglidio_FreeShippingPromo/free-shipping-banner",
             tracks: {
-                message: true,
                 subtotal: true
             }
         },
@@ -36,6 +35,21 @@ define([
             cart.subscribe(cart => {
                 if (!_.isEmpty(cart) && !_.isUndefined(cart.subtotalAmount)) {
                     self.subtotal = parseFloat(cart.subtotalAmount);
+                }
+            });
+
+            self.message = ko.computed(function() {
+                if (_.isUndefined(self.subtotal) || self.subtotal === 0) {
+                    return self.messageDefault;
+                }
+
+                if (self.subtotal > 0 && self.subtotal < self.freeShippingThreshold) {
+                    var subtotalRemaining = self.freeShippingThreshold - self.subtotal;
+                    return self.messageItemsInCart.replace('$XX.XX', self.formatCurrency(subtotalRemaining));
+                }
+
+                if (self.subtotal >= self.freeShippingThreshold) {
+                    return self.messageFreeShipping;
                 }
             });
             
