734ca305b7702d6386cd7f6f688e815f2fdbf121
Lucas Figueiredo <lucas-fj@hotmail.com>
2021-11-06 19:34:15 +0100

    Import an external property value into a UI Component

---
+++ CheckoutMessages/view/frontend/web/js/view/summary/shipping-time.js
@@ -0,0 +1,21 @@
+define([
+    'uiComponent'
+], function(
+    Component
+){
+    'use strict';
+
+    return Component.extend({
+        defaults: {
+            tracks: {
+                countryId: true
+            },
+            imports: {
+                countryId: 'checkoutProvider:shippingAddress.country_id'
+            }
+        },
+        showMessage: function() {
+            return this.countryId === 'US';
+        }
+    });
+});
